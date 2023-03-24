3f56574aa4526e53394b75e3ae7eac69a92868cf
Nitanta Adhikari <nitanta.adh007@gmail.com>
2023-03-24 14:39:10 +0545

    Initial Commit

---
+++ Settingbundle/SettingbundleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingbundleApp.swift
+//  Settingbundle
+//
+//  Created by Nitanta Adhikari on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SettingbundleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
